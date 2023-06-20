2b144e966721f33ec08ff069a8bac208e0334910
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-19 20:05:49 -0500

    Creating the SearchForm complete

--- src/components/RecipeSearchForm.vue
+++ src/components/RecipeSearchForm.vue
@@ -1,7 +1,32 @@
-<script setup></script>
+<script setup>
+import Button from 'primevue/button'
+import Chips from 'primevue/chips'
+import Fieldset from 'primevue/fieldset'
+import { ref } from 'vue'
+import { search } from '../stores/recipeStore'
+import { RecipeSearchResults } from './RecipeSearchResults.vue'
+
+const ingredients = ref([])
+const handleSubmit = () => {
+  search(ingredients.value)
+}
+</script>
 
 <template>
-  <div>RecipeSearchForm</div>
+  <Fieldset legend="Search Recipes" :toggleable="true">
+    <form class="mt-4" @submit.prevent="handleSubmit">
+      <span class="p-float-label">
+        <Chips id="chips" v-model="ingredients" class="block" />
+        <label for="chips">Ingredients</label>
+      </span>
+      <div class="mt-4">
+        <Button :disabled="!ingredients.length" type="submit">Search Recipes</Button>
+      </div>
+    </form>
+    <div class="mt-6">
+      <RecipeSearchForm />
+    </div>
+  </Fieldset>
 </template>
 
 <style scoped></style>
