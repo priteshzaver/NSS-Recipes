b154e491734f836fd78435614e1e1f4767fbc91b
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-20 11:01:41 -0500

    Final Touches complete

--- src/stores/favoritesStore.js
+++ src/stores/favoritesStore.js
@@ -1,11 +1,19 @@
 import { ref } from 'vue'
 
-export const favorites = ref([])
+const localStorageFavorites = JSON.parse(localStorage.getItem('recipes'))
+
+export const favorites = ref(localStorageFavorites || [])
+
+const saveFavorites = () => {
+  localStorage.setItem('recipes', JSON.stringify(favorites.value))
+}
 
 export const addToFavorites = (recipe) => {
   favorites.value = [...favorites.value, recipe]
+  saveFavorites()
 }
 
 export const removeFavorite = (id) => {
   favorites.value = favorites.value.filter((r) => r.recipeId !== id)
+  saveFavorites()
 }
